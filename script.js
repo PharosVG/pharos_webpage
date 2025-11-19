78d9e546f71d4f562df76a4205dd8a189c0ea795
NokkelDag <pharos.vg@gmail.com>
2025-11-19 15:47:20 +0100

    pulido

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-// Pequeña animación de bienvenida
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Página cargada correctamente 🎉");
-  //alert("¡Bienvenido a mi página de prueba!");
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+const header = document.querySelector('header');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    header.classList.toggle('menu-open');
 });
 
+const botones = [
+    'btn-infante',
+    'btn-adulto',
+    'btn-pareja',
+    'btn-sexologia',
+    'btn-drogodependencia',
+    'btn-deporte',
+    'btn-taller1',
+    'btn-taller2'
+];
+
+botones.forEach(id => {
+    const boton = document.getElementById(id);
+    if (boton) {
+        boton.addEventListener('click', () => {
+            window.location.href = "construccion.html";
+        });
+    }
+});
