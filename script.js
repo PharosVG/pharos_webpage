73d1f3e950076d3d4016590a30883918d59ad5c7
PharosVG <pharos.vg@gmail.com>
2025-11-19 01:17:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Pequeña animación de bienvenida
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Página cargada correctamente 🎉");
+  //alert("¡Bienvenido a mi página de prueba!");
+});
+
